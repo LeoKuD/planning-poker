c2ff497389b688af2e9215e4ab74a35dde96324a
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-09-05 16:52:20 +0300

    init: start pointing-poker (server-config)

---
+++ server/src/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Start  building..."', () => {
+      expect(appController.getWelcomeMsg().msg)
+        .toBe('Start building your Postgres-Nest-React Application');
+    });
+  });
+});
