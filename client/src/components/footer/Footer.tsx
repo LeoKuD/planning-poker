9d72980bf9d33215298ea3b0d3769901b824c3dc
leokud <then7w@gmail.com>
2021-09-11 21:04:47 +0300

    refactor

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,38 +1,29 @@
 import React, { FC } from 'react';
-import s from './Footer.module.css';
+import styles from './Footer.module.css';
 import logo from '../../assets/img/rs_school_js.svg';
 
 export const Footer: FC = () => {
-  interface IgitHubLinks {
-    name: string,
-    link: string,
-    id: number
-  }
-  const gitHubLinks : IgitHubLinks[] = [
-    {
-      name: 'github.com/LeoKuD',
-      link: 'https://github.com/LeoKuD',
-      id: 0,
-    },
-    {
-      name: 'github.com/kastrubait',
-      link: 'https://github.com/kastrubait',
-      id: 1,
-    },
-    {
-      name: 'github.com/kisaragi99',
-      link: 'ttps://github.com/kisaragi99',
-      id: 2,
-    }];
+  const gitHubNames: string[] = ['LeoKuD', 'kastrubait', 'kisaragi99'];
   return (
-    <footer className={s.footer}>
-      <div className={s.footer_logo}><a href="https://rs.school/react/"><img src={logo} alt="Logo RSS" /></a></div>
-      <div className={s.footer_text}>2021</div>
-      <div className={s.footer_links}>
+    <footer className={styles.footer}>
+      <figure className={styles.footer_logo}>
+        <a href="https://rs.school/react/">
+          <img src={logo} alt="Logo RSS" />
+        </a>
+      </figure>
+      <p className={styles.footer_text}>2021</p>
+      <nav className={styles.footer_links}>
         <ul>
-          {gitHubLinks.map((item : IgitHubLinks) => <li key={item.id}><a href={item.link}>{item.name}</a></li>)}
+          {gitHubNames.map((item: string) => (
+            <li key={item}>
+              <a href={`https://github.com/${item}`}>
+                github.com/
+                {item}
+              </a>
+            </li>
+          ))}
         </ul>
-      </div>
+      </nav>
     </footer>
   );
 };
