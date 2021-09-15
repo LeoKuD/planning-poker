26538b4a1edef574ef346247abae691ccce7f5e7
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-09-15 22:59:12 +0300

    refactor: add utilities for converting terms and show numbers as percent

--- client/src/results/components/RoundStatistics/index.tsx
+++ client/src/results/components/RoundStatistics/index.tsx
@@ -1,6 +1,7 @@
-import Card from 'components/Card';
 import React, { FC } from 'react';
+import Card from 'components/Card';
 import { RoundScore } from 'types/common-types';
+import { cardName, cardScore, percent } from 'utils';
 import style from './index.module.scss';
 
 interface RoundStatisticsProps {
@@ -8,17 +9,8 @@ interface RoundStatisticsProps {
 }
 
 const RoundStatistics: FC<RoundStatisticsProps> = ({ roundData }) => {
-  const option = {
-    style: 'percent',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  };
-  const percent = new Intl.NumberFormat('en-US', option);
-  const cardName = (value: string): string => value.split('-')[0];
-  const cardScore = (value: string): number | null => parseInt(value.split('-')[1], 10);
-
   const statisticsList = Object.entries(roundData.score).map(([key, value]) => (
-    <li>
+    <li key={`${key}${new Date().getTime()}`}>
       <Card
         shortName={cardName(key).toUpperCase()}
         cardScore={cardScore(key)}
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+const option = {
+  style: 'percent',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+};
+
+export const percent = new Intl.NumberFormat('en-US', option);
+
+export const cardName = (value: string): string => value.split('-')[0];
+
+export const cardScore = (value: string): number | null => parseInt(value.split('-')[1], 10);
+
 export const setLocalStorageToken = (token: string): void => localStorage.setItem(
     process.env.REACT_APP_LOCAL_TOKEN as string,
     token,
