5748835ba1890ba6241ea00f0e37226f2a2c77d1
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-09-16 14:57:01 +0300

    feat: implement ResultPage component (no styles)

--- client/src/pages/Results/index.tsx
+++ client/src/pages/Results/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { getRoundScore } from 'api/mock-api.service';
+import { getLobbyIssues, getRoundScore } from 'api/mock-api.service';
 import RoundStatistics from 'results/components/RoundStatistics';
-import { RoundScore } from 'types/common-types';
+import SessionHeader from 'session/components/SessionHeader';
+import { IssueCard } from 'components/Issue';
+import { Issue, RoundScore } from 'types/common-types';
+import style from './index.module.scss';
 
 const Results: React.FC<HTMLElement> = () => {
-  const roundData = getRoundScore('IU43E', 19) as RoundScore;
+  const sessionId = 'IU43E';
+  const issues = getLobbyIssues(sessionId);
   return (
-    <>
-      <RoundStatistics roundData={roundData} />
-    </>
+    <div>
+      <SessionHeader sessionId={sessionId} modeMaster={false} />
+      <ul>
+        {issues.map((item: Issue) => (
+          <li className={style.resultRound}>
+            <IssueCard issueData={item} />
+            <RoundStatistics roundData={getRoundScore(sessionId, item.id) as RoundScore} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
