479ef0c6d8275bc5cc44e52b9d40eb3080e27fb3
kisaragi99 <budzhalov99@gmail.com>
2021-09-17 11:00:19 +0300

    fix: added create new issue button

--- client/src/session/components/SessionIssues/index.tsx
+++ client/src/session/components/SessionIssues/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
 import SectionHeader from 'components/SectionHeader';
 import { IssueCard } from 'components/Issue';
-import { IssueData } from 'types/common-types';
+import { IssueData, ButtonTypes } from 'types/common-types';
 import IssueInfo from 'components/IssueInfo';
+import { Button } from 'components/Button';
 import styles from './index.module.scss';
 
 type SessionIssuesProps = {
@@ -14,6 +15,9 @@ const SessionIssues: FC<SessionIssuesProps> = ({ issues, modeMaster }) => {
   if (modeMaster) {
     // add create new issue button
   }
+  const addNewIssue = (): void => {
+    console.log('new issue added');
+  };
 
   return (
     <div className={styles.container}>
@@ -25,6 +29,9 @@ const SessionIssues: FC<SessionIssuesProps> = ({ issues, modeMaster }) => {
             modeMaster={modeMaster}
           />
         ))}
+        <Button onClick={addNewIssue} type={ButtonTypes.primary}>
+          Create new issue
+        </Button>
       </div>
       <IssueInfo />
     </div>
