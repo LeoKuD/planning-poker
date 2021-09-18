053812cb93fe689b1a767a3d1efb83a172b789f7
kisaragi99 <budzhalov99@gmail.com>
2021-09-18 12:18:48 +0300

    fix: changed SectionHeader to SessionHeader in SessionInfo

--- client/src/session/components/SessionInfo/index.tsx
+++ client/src/session/components/SessionInfo/index.tsx
@@ -1,19 +1,19 @@
 import React, { FC } from 'react';
-import SessionHeader from 'components/SectionHeader';
+import SessionHeader from 'session/components/SessionHeader';
 import User from 'components/User';
 import { UserData, ButtonTypes } from 'types/common-types';
 import { Button } from 'components/Button';
 import style from './index.module.scss';
 
 type SessionInfoProps = {
-    lobbyInfo: string,
     user: UserData,
     link: string,
+    sessionId: string,
     settingsMode?: boolean
 }
 
 const SessionInfo: FC<SessionInfoProps> = ({
-  lobbyInfo = '', user, link, settingsMode,
+  user, link, sessionId, settingsMode = false,
 }) => {
   const copyToClipboard = async (): Promise<void> => {
     await navigator.clipboard.writeText(link);
@@ -23,7 +23,7 @@ const SessionInfo: FC<SessionInfoProps> = ({
   return (
     <div className={style.container}>
       <div className={style.container__header}>
-        <SessionHeader header={lobbyInfo} />
+        <SessionHeader sessionId={sessionId} modeMaster={settingsMode} />
       </div>
       <div className={style.container__user}>
         <label htmlFor="scramMaster">Scram Master:</label>
