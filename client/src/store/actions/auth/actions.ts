d944888738b94e67545bbee3c81fca49e5e5724a
LeoKuD <then7w@gmail.com>
2021-09-27 15:46:41 +0300

    refactor: add new reducer and api for socket

--- client/src/store/actions/auth/actions.ts
+++ client/src/store/actions/auth/actions.ts
@@ -1,4 +1,5 @@
 import { Dispatch } from 'react';
+import { User } from 'store/reducers/reducer';
 import { Action } from 'types/index';
 import * as utils from 'utils';
 // import { Action } from 'types';
@@ -44,3 +45,26 @@ export const getProfile = (history: any) => async (dispatch: Dispatch<any>) => {
     dispatch(userLoggedOut());
   }
 };
+export const createLobbyAC = (state: any): Action => ({
+  type: types.CREATE_SESSION,
+  payload: state,
+});
+
+export const setSessionValidAC = (data: any): Action => ({
+  type: types.SET_SESSION_VALID,
+  payload: data,
+});
+
+export const setConnectionAC = (data: any): Action => ({
+  type: types.SET_CONNECTION,
+  payload: data,
+});
+
+export const addNewUserAC = (newUser: User): Action => ({
+  type: types.ADD_NEW_USER,
+  payload: newUser,
+});
+export const startSessionAC = (sessionData: User): Action => ({
+  type: types.START_SESSION,
+  payload: sessionData,
+});
--- client/src/store/actions/auth/types.ts
+++ client/src/store/actions/auth/types.ts
@@ -10,3 +10,9 @@ export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILE_SUCCESS = 'GET_PROFILE_SUCCESS';
 
 export const LOGOUT = 'LOGOUT';
+
+export const CREATE_SESSION = 'CREATE_SESSION';
+export const SET_SESSION_VALID = 'SET_SESSION_VALID';
+export const SET_CONNECTION = 'SET_CONNECTION';
+export const ADD_NEW_USER = 'ADD_NEW_USER';
+export const START_SESSION = 'START_SESSION';
