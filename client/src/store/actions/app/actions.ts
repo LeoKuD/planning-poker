12b3317822d4cf84a9102f378511264f66fc5225
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2021-09-29 06:17:44 +0300

    fix: remove duplicate, missin links

--- client/src/store/actions/app/actions.ts
+++ client/src/store/actions/app/actions.ts
@@ -3,25 +3,25 @@ import { Action } from 'types/index';
 import * as types from './types';
 
 export const createLobbyAC = (state: any): Action => ({
-    type: types.CREATE_SESSION,
-    payload: state,
-  });
-  
-  export const setSessionValidAC = (data: any): Action => ({
-    type: types.SET_SESSION_VALID,
-    payload: data,
-  });
-  
-  export const setConnectionAC = (data: any): Action => ({
-    type: types.SET_CONNECTION,
-    payload: data,
-  });
-  
-  export const addNewUserAC = (newUser: User): Action => ({
-    type: types.ADD_NEW_USER,
-    payload: newUser,
-  });
-  export const startSessionAC = (sessionData: User): Action => ({
-    type: types.START_SESSION,
-    payload: sessionData,
-  });
+  type: types.CREATE_SESSION,
+  payload: state,
+});
+
+export const setSessionValidAC = (data: any): Action => ({
+  type: types.SET_SESSION_VALID,
+  payload: data,
+});
+
+export const setConnectionAC = (data: any): Action => ({
+  type: types.SET_CONNECTION,
+  payload: data,
+});
+
+export const addNewUserAC = (newUser: User): Action => ({
+  type: types.ADD_NEW_USER,
+  payload: newUser,
+});
+export const startSessionAC = (sessionData: User): Action => ({
+  type: types.START_SESSION,
+  payload: sessionData,
+});
